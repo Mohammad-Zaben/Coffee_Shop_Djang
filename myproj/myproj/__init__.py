89840c98f49909c1d1a1e901302d1ced207e53d2
Zaben <141401082+Mohammad-Zaben@users.noreply.github.com>
2024-08-25 16:44:29 +0300

    database2 commit

--- myproj/myproj/__init__.py
+++ myproj/myproj/__init__.py
@@ -1,18 +1,22 @@
 # from django.db import connection
 #
-# # Define your raw SQL query
-# query = """
-#             LOAD DATA INFILE 'C:/Users/Lenovo/Downloads/201904 sales reciepts.csv'
-#             INTO TABLE polls_salesreceipts
-#             FIELDS TERMINATED BY ','  -- Specify the delimiter, typically a comma for CSV files
-#             ENCLOSED BY '"'           -- Specify if fields are enclosed by a character, typically double quotes
-#             LINES TERMINATED BY '\n'  -- Specify the line terminator, typically a newline character
-#             IGNORE 1 LINES            -- Skip the header row in the CSV file
-# (transaction_id,transaction_date,transaction_time,instore_yn,customer_id,product_id,sales_outlet_id,line_item_amount,line_item_id,order,promo_item_yn,quantity,staff_id,unit_price)
-# """
-# # Execute the query
+# # Disable foreign key checks
 # with connection.cursor() as cursor:
-#     cursor.execute(query)
-#     results = cursor.fetchall()
+#     cursor.execute("SET FOREIGN_KEY_CHECKS=0;")
 #
-# # Print the results
+#     # Your LOAD DATA INFILE query
+#     query = """
+#         LOAD DATA INFILE 'C:/Users/Lenovo/Downloads/201904 sales reciepts.csv'
+#         INTO TABLE polls_salesreceipts
+#         FIELDS TERMINATED BY ','
+#         ENCLOSED BY '"'
+#         LINES TERMINATED BY '\n'
+#         IGNORE 1 LINES
+#         (`transaction_id`, `transaction_date`, `transaction_time`, `staff_id`, `instore_yn`, `order`,
+#          `line_item_id`, `quantity`, `line_item_amount`, `unit_price`, `promo_item_yn`,
+#          `customer_id`, `product_id`, `sales_outlet_id`)
+#     """
+#     cursor.execute(query)
+#
+#     # Re-enable foreign key checks
+#     cursor.execute("SET FOREIGN_KEY_CHECKS=1;")
--- myproj/polls/models/SalesReceipts.py
+++ myproj/polls/models/SalesReceipts.py
@@ -5,19 +5,25 @@ from .Product import Product
 
 
 class SalesReceipts(models.Model):
-    transaction_id = models.IntegerField(primary_key=True)
+    transaction_id = models.IntegerField()
     transaction_date = models.DateField()
     transaction_time = models.DateTimeField()
     sales_outlet = models.ForeignKey(SalesOutlet, on_delete=models.CASCADE)
-    staff_id = models.IntegerField(default=0)
+    staff_id = models.IntegerField()
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     instore_yn = models.CharField(max_length=1)
-    order = models.IntegerField(default=0)
-    line_item_id = models.IntegerField(default=0)
+    order = models.IntegerField()
+    line_item_id = models.IntegerField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=0)
-    line_item_amount = models.DecimalField(max_digits=4, decimal_places=2,default=0.0)
-    unit_price = models.DecimalField(max_digits=4, decimal_places=2,default=0.0)
-    promo_item_yn = models.CharField(max_length=1,default='N')
+    quantity = models.IntegerField()
+    line_item_amount = models.DecimalField(max_digits=4, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=4, decimal_places=2)
+    promo_item_yn = models.CharField(max_length=1, default='N')
 
-
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['transaction_id', 'transaction_date', 'transaction_time'],
+                name='unique_migration_host_combination'
+            )
+        ]
