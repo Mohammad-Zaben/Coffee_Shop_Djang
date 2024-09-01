7dfabba0fa661281890fd19d7895170090abf6a4
Tala Jebrini <tjebrini8@gmail.com>
2024-09-01 10:08:52 +0300

    sec Asana commit

--- myproj/polls/Asana.py
+++ myproj/polls/Asana.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from sqlalchemy import create_engine
 import pandas as pd
 
-
 engine = create_engine('mysql+pymysql://root:@localhost:3306/test_data')
 df = pd.read_sql('SELECT * FROM salereciepts', con=engine)
 df['transaction_date'] = pd.to_datetime(df['transaction_date'])
@@ -13,28 +12,36 @@ def sales_daily_base(sales_outlet_id, day_date):
     new_df = df[sales_list]
 
     new_df1 = new_df[(new_df["transaction_date"] == day_date) & (new_df["sales_outlet_id"] == sales_outlet_id)]
-    new_df1.loc[:, "total_sales"] = new_df1["quantity"] * new_df1["unit_price"]
-
+    new_df1 = new_df1.assign(total_sales=lambda x: (x['quantity'] * x['unit_price']))
     # Print the total sales sum
-    print(new_df1["total_sales"].sum())
+    return new_df1["total_sales"].sum()
 
-def daily_logic(date_range,days_num,sales_outlet_id):
-    for i in days_num:
-        sales_daily_base(sales_outlet_id,date_range[i].to_pydatetime())
 
-def logic_type(start_date,end_date,sales_outlet_id):
+def daily_logic(date_range, days_num, sales_outlet_id):
+    total_sales = 0
+    for i in range(days_num):
+        total_sales = total_sales + sales_daily_base(sales_outlet_id, date_range[i].to_pydatetime())
+    return total_sales
+
+
+def weekly_logic(start_date, end_date, sales_outlet_id):
+    date_range = pd.date_range(start=start_date, end=end_date, freq='W')
+
+
+def logic_type(start_date, end_date, sales_outlet_id):
     date_range = pd.date_range(start=start_date, end=end_date)
     days_num = len(date_range)
-    if(days_num <= 7):
-        daily_logic(date_range,days_num,sales_outlet_id)
-    elif (days_num <= 30):
+    total_sales = 0
+    if (days_num <= 7):
+        total_sales = daily_logic(date_range, days_num, sales_outlet_id)
+    elif (days_num < 30):
         pass
     elif (days_num <= 365):
         pass
     else:
         pass
 
-
+    return total_sales
 
 
 if __name__ == '__main__':
@@ -42,5 +49,5 @@ if __name__ == '__main__':
     end_date = '2019-04-05'
     sales_outlet_id = 3
 
-    logic_type(start_date, end_date, sales_outlet_id)
-
+    x = logic_type(start_date, end_date, sales_outlet_id)
+    print(x)
