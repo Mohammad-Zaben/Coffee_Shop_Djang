ab6707c75154cf6dd531334a5e704dd714ced1c7
Zaben <141401082+Mohammad-Zaben@users.noreply.github.com>
2024-09-01 12:11:03 +0300

    week commit

--- myproj/polls/Asana.py
+++ myproj/polls/Asana.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from sqlalchemy import create_engine
 import pandas as pd
 
-engine = create_engine('mysql+pymysql://root:@localhost:3306/test_data')
+engine = create_engine('mysql+pymysql://root:@localhost:3306/test data')
 df = pd.read_sql('SELECT * FROM salereciepts', con=engine)
 df['transaction_date'] = pd.to_datetime(df['transaction_date'])
 
@@ -19,24 +19,40 @@ def sales_daily_base(sales_outlet_id, day_date):
 
 def daily_logic(date_range, days_num, sales_outlet_id):
     total_sales = 0
+    df = pd.DataFrame(columns=['Date', 'total_sales'])
     for i in range(days_num):
-        total_sales = total_sales + sales_daily_base(sales_outlet_id, date_range[i].to_pydatetime())
-    return total_sales
+        total_sales = sales_daily_base(sales_outlet_id, date_range[i].to_pydatetime())
+        new_row = pd.DataFrame({'Date': [date_range[i].to_pydatetime()], 'total_sales': [total_sales]})
+        df = pd.concat([df, new_row], ignore_index=True)
+        # total_sales = total_sales + sales_daily_base(sales_outlet_id, date_range[i].to_pydatetime())
+
+    return df
 
 
 def weekly_logic(start_date, end_date, sales_outlet_id):
-    date_range = pd.date_range(start=start_date, end=end_date, freq='W')
+    sales_list = ['transaction_date', 'sales_outlet_id', 'quantity', 'unit_price']
+    new_df = df[sales_list]
+    new_df1 = new_df[(new_df["transaction_date"] >= start_date) & (new_df["transaction_date"] <= end_date) & (
+            new_df["sales_outlet_id"] == sales_outlet_id)]
+
+    print(new_df1.dtypes)
+    new_df1 = new_df1.assign(work_week=lambda x: x["transaction_date"].apply(lambda dt: dt.isocalendar()[1]))
+    print(new_df1)
+
+    return 0
+
+    # new_df1 = new_df1.assign(total_sales=lambda x: (x['quantity'] * x['unit_price']))
 
 
 def logic_type(start_date, end_date, sales_outlet_id):
     date_range = pd.date_range(start=start_date, end=end_date)
     days_num = len(date_range)
     total_sales = 0
-    if (days_num <= 7):
+    if days_num <= 7:
         total_sales = daily_logic(date_range, days_num, sales_outlet_id)
-    elif (days_num < 30):
-        pass
-    elif (days_num <= 365):
+    elif days_num <= 30:
+        weekly_logic(start_date, end_date, sales_outlet_id)
+    elif days_num <= 365:
         pass
     else:
         pass
@@ -46,7 +62,7 @@ def logic_type(start_date, end_date, sales_outlet_id):
 
 if __name__ == '__main__':
     start_date = '2019-04-01'
-    end_date = '2019-04-05'
+    end_date = '2019-04-04'
     sales_outlet_id = 3
 
     x = logic_type(start_date, end_date, sales_outlet_id)
