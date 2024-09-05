77e624e461c24ed152e74aaf840c1ed2a8fd622b
Mohammad Zaben <141401082+Mohammad-Zaben@users.noreply.github.com>
2024-09-05 16:33:49 +0300

    Update sales_aggregator.py

--- myproj/polls/controllers/sales_aggregator.py
+++ myproj/polls/controllers/sales_aggregator.py
@@ -9,28 +9,16 @@ def filter_df(sales_outlet_id, start_date, end_date):
     return filtered_df
 
 
-def apply_total_sales(filtered_df):
-    filtered_df = filtered_df.assign(total_sales=lambda x: (x['quantity'] * x['unit_price']))
-    return filtered_df
-
-
-def get_work_week_df(sales_df):
-    sales_df = sales_df.assign(work_week=lambda x: x["transaction_date"].apply(lambda dt: dt.isocalendar()[1]))
-    return sales_df
-
-
 def get_total_sales(sales_outlet_id, start_date, end_date, logic_type):
     sales_df = filter_df(sales_outlet_id, start_date, end_date)
-    sales_df = apply_total_sales(sales_df)
+    sales_df = Filter.apply_total_sales(sales_df)
     if logic_type == 'daily':
         sales_df = sales_df.groupby(['transaction_date'])['total_sales'].sum().reset_index()
         return sales_df
     elif logic_type == 'weekly':
-        sales_df = get_work_week_df(sales_df)
+        sales_df = Filter.get_work_week_df(sales_df)
         sales_df.drop(['transaction_date'], axis=1, inplace=True)
         sales_df = sales_df.groupby(['work_week'])['total_sales'].sum().reset_index()
         return sales_df
     else:
         print('Invalid logic type')
-
-
