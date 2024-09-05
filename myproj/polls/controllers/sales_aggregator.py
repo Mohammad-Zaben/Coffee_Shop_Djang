7fbf4e46991653015929853111dcee3b78ee9311
Tala Jebrini <tjebrini8@gmail.com>
2024-09-05 14:09:43 +0300

    2nd review commit

--- myproj/polls/controllers/sales_aggregator.py
+++ myproj/polls/controllers/sales_aggregator.py
@@ -1,12 +1,11 @@
 from myproj.polls.controllers.database import sale_receipts_df
+from myproj.polls.controllers.Filtering import Filter
 
 
 def filter_df(sales_outlet_id, start_date, end_date):
     sales_list = ['transaction_date', 'sales_outlet_id', 'quantity', 'unit_price']
-    filtered_df = sale_receipts_df[sales_list]
-    filtered_df = filtered_df[
-        (filtered_df["transaction_date"] >= start_date) & (filtered_df["transaction_date"] <= end_date) & (
-                filtered_df["sales_outlet_id"] == sales_outlet_id)]
+    filtered_df = Filter.filter_df(sales_outlet_id=sales_outlet_id, start_date=start_date, end_date=end_date,
+                                   filtering_list=sales_list)
     return filtered_df
 
 
@@ -23,15 +22,16 @@ def get_work_week_df(sales_df):
 def get_total_sales(sales_outlet_id, start_date, end_date, logic_type):
     sales_df = filter_df(sales_outlet_id, start_date, end_date)
     sales_df = apply_total_sales(sales_df)
-    sales_df.drop(['sales_outlet_id', 'quantity', 'unit_price'], axis=1, inplace=True)
-    #print(sales_df.sort_values(by='transaction_date'))
     if logic_type == 'daily':
-        sales_df = sales_df.groupby(['transaction_date']).sum().reset_index()
+        sales_df = sales_df.groupby(['transaction_date'])['total_sales'].sum().reset_index()
         return sales_df
     elif logic_type == 'weekly':
         sales_df = get_work_week_df(sales_df)
         sales_df.drop(['transaction_date'], axis=1, inplace=True)
-        sales_df = sales_df.groupby(['work_week']).sum().reset_index()
+        sales_df = sales_df.groupby(['work_week'])['total_sales'].sum().reset_index()
         return sales_df
     else:
         print('Invalid logic type')
+
+
+print(get_total_sales(3, '2019-04-01', '2019-04-05', 'daily'))
